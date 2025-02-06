e891a288a4e6f0afa06837dad8ba62ea00885f81
Tiago Miranda <tsilva@ionic.health>
2025-02-06 20:33:21 -0300

    feat: add new tcp static simulation

--- TCPconnection/TCPstatic1.cc
+++ TCPconnection/TCPstatic1.cc
@@ -7,62 +7,53 @@
 #include "ns3/bridge-module.h"
 #include "ns3/flow-monitor-module.h"
 #include "ns3/point-to-point-module.h"
-#include "ns3/on-off-helper.h"
-#include "ns3/netanim-module.h"
 
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("TrabalhoRedes");
 
-int main(int argc, char *argv[]) {
-  // Configuração de logs (não necessariamente usados, mas permanecem)
-  // LogComponentEnable("TrabalhoRedes", LOG_LEVEL_INFO);
-  // LogComponentEnable("StaWifiMac", LOG_LEVEL_INFO);
-  // LogComponentEnable("ApWifiMac", LOG_LEVEL_INFO);
-
-  // Criando o nó servidor (s0)
+int main (int argc, char *argv[])
+{
+  // Criação dos nós
   NodeContainer serverNode;
   serverNode.Create(1);
 
-  // Criando o nó Access Point (AP)
   NodeContainer apNode;
   apNode.Create(1);
 
-  // Criando os clientes sem fio (por exemplo, 32 clientes)
   NodeContainer wifiStaNodes;
   wifiStaNodes.Create(32);
 
-  // Configurando o canal Wi-Fi
+  // Configurando o canal e PHY do Wi-Fi
   YansWifiChannelHelper channel = YansWifiChannelHelper::Default();
   YansWifiPhyHelper phy = YansWifiPhyHelper();
   phy.SetPcapDataLinkType(YansWifiPhyHelper::DLT_IEEE802_11);
   phy.SetChannel(channel.Create());
 
-  // Configurando o dispositivo Wi-Fi
+  // Configurando os dispositivos Wi-Fi
   WifiHelper wifi;
-  wifi.SetRemoteStationManager("ns3::ConstantRateWifiManager",
-                               "DataMode", StringValue("HtMcs7"),
+  wifi.SetRemoteStationManager("ns3::ConstantRateWifiManager", 
+                               "DataMode", StringValue("HtMcs7"), 
                                "ControlMode", StringValue("HtMcs0"));
 
   WifiMacHelper mac;
   Ssid ssid = Ssid("EquipeX");
 
   // Configuração do AP
-  mac.SetType("ns3::ApWifiMac",
-              "Ssid", SsidValue(ssid));
+  mac.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid));
   NetDeviceContainer apDevice = wifi.Install(phy, mac, apNode);
 
-  // Configuração dos clientes
+  // Configuração dos clientes STA
   mac.SetType("ns3::StaWifiMac", "Ssid", SsidValue(ssid));
   NetDeviceContainer staDevices = wifi.Install(phy, mac, wifiStaNodes);
 
-  // Instalando a pilha de Internet
+  // Instalando a pilha de protocolos (Internet)
   InternetStackHelper stack;
   stack.Install(serverNode);
   stack.Install(apNode);
   stack.Install(wifiStaNodes);
 
-  // Habilitar IP Forwarding no AP (para roteamento entre interfaces)
+  // Habilitar IP Forwarding no AP para roteamento entre interfaces
   Ptr<Ipv4> ipv4 = apNode.Get(0)->GetObject<Ipv4>();
   ipv4->SetAttribute("IpForward", BooleanValue(true));
 
@@ -78,20 +69,17 @@ int main(int argc, char *argv[]) {
   p2p.SetChannelAttribute("Delay", StringValue("2ms"));
   NetDeviceContainer p2pDevices = p2p.Install(apNode.Get(0), serverNode.Get(0));
 
-  // Atribuindo endereços IP ao link ponto a ponto
   Ipv4AddressHelper p2pAddress;
   p2pAddress.SetBase("10.1.1.0", "255.255.255.0");
   Ipv4InterfaceContainer p2pInterfaces = p2pAddress.Assign(p2pDevices);
 
-  // Configurando a mobilidade dos nós
+  // Configurando a mobilidade dos nós (clientes e AP estáticos)
   MobilityHelper mobility;
   mobility.SetPositionAllocator("ns3::GridPositionAllocator",
                                 "MinX", DoubleValue(0.0),
                                 "MinY", DoubleValue(0.0),
-                                // Delta
                                 "DeltaX", DoubleValue(2.0),
                                 "DeltaY", DoubleValue(4.0),
-                                // Tamanho da grade
                                 "GridWidth", UintegerValue(8),
                                 "LayoutType", StringValue("RowFirst"));
   mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
@@ -99,37 +87,33 @@ int main(int argc, char *argv[]) {
   mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
   mobility.Install(wifiStaNodes);
 
-  // Aplicação UDP no servidor (escuta na porta 9)
-  uint16_t port = 9;
-  PacketSinkHelper sinkHelper("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), port));
+  // Aplicação TCP: configurando o servidor
+  // O servidor usará um PacketSink que escuta por conexões TCP na porta 50000.
+  uint16_t port = 50000;
+  PacketSinkHelper sinkHelper("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), port));
   ApplicationContainer serverApp = sinkHelper.Install(serverNode.Get(0));
   serverApp.Start(Seconds(1.0));
   serverApp.Stop(Seconds(30.0));
 
-  // Aplicação UDP nos clientes – enviar para o servidor (p2pInterfaces.GetAddress(1))
-  OnOffHelper onOffHelper("ns3::UdpSocketFactory", InetSocketAddress(p2pInterfaces.GetAddress(1), port));
-  onOffHelper.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=1]"));
-  onOffHelper.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0]"));
-  onOffHelper.SetAttribute("DataRate", StringValue("1Mbps"));
-  onOffHelper.SetAttribute("PacketSize", UintegerValue(1024));
-  ApplicationContainer clientApps = onOffHelper.Install(wifiStaNodes);
+  // Aplicação TCP: configurando os clientes para enviar dados ao servidor
+  // Utilizamos BulkSendHelper para enviar dados via TCP (o atributo "MaxBytes" com valor 0 indica envio ilimitado)
+  BulkSendHelper bulkSend("ns3::TcpSocketFactory", InetSocketAddress(p2pInterfaces.GetAddress(1), port));
+  bulkSend.SetAttribute("MaxBytes", UintegerValue(0)); // sem limite de bytes
+  ApplicationContainer clientApps = bulkSend.Install(wifiStaNodes);
   clientApps.Start(Seconds(2.0));
   clientApps.Stop(Seconds(30.0));
 
-  // Habilitar roteamento global
+  // Populando as tabelas de roteamento
   Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
-  // Configurar o Flow Monitor
-  Ptr<FlowMonitor> flowMonitor;
+  // Configurando o Flow Monitor para coletar estatísticas
   FlowMonitorHelper flowHelper;
-  flowMonitor = flowHelper.InstallAll();
-
-  AnimationInterface anim("UDPstatic1.xml");
+  Ptr<FlowMonitor> flowMonitor = flowHelper.InstallAll();
 
   Simulator::Stop(Seconds(40.0));
   Simulator::Run();
 
-  flowMonitor->SerializeToXmlFile("flow-monitor.xml", true, true);
+  flowMonitor->SerializeToXmlFile("TCP-static.xml", true, true);
   Simulator::Destroy();
 
   return 0;
